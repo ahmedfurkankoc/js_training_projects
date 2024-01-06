8528453a4038f519bb97b9f033f177542c1cb884
Ahmed Furkan <ahmedfurkankoc@gmail.com>
2024-01-06 13:53:20 +0300

    some changes

--- todo_list/script.js
+++ todo_list/script.js
@@ -1,51 +1,112 @@
+
+"use strict";
+        
 let taskList = [
     {"id": 1, "taskName": "Task 1"},
     {"id": 2, "taskName": "Task 2"},
-    {"id": 3, "taskName": "Task 3"},
+    {"id": 7, "taskName": "Task 3"},
     {"id": 4, "taskName": "Task 4"},
 ];
 
+let editId;
+let isEditTask = false;
+
+const taskInput = document.querySelector("#txtTaskName");
+const btnClear = document.querySelector("#btnClear");
+
 displayTasks();
 
-function displayTasks(){
-
+function displayTasks() {
     let ul = document.getElementById("task-list");
-
     ul.innerHTML = "";
 
-    for(let task of taskList){
-        let li = `
-            <li class="task list-group-item">
-                <div class="form-check">
-                    <input type="checkbox" id="${task.id}" class="form-check-input">
-                    <label for="${task.id}" class="form-check-label">${task.taskName}</label>
-                </div>
-            </li>
-        `;
+    if (taskList.length == 0) {
+        ul.innerHTML = "<p class='p-3 m-0'>Task list empty</p>"
+    } else {
 
-        ul.insertAdjacentHTML("beforeend", li);
+        for(let task of taskList) {
+
+            let li = `
+                <li class="task list-group-item">
+                    <div class="form-check">
+                        <input type="checkbox" id="${task.id}" class="form-check-input">
+                        <label for="${task.id}" class="form-check-label">${task.taskName}</label>
+                    </div>
+                    <div class="dropdown">
+                        <button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fa-solid fa-ellipsis"></i>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a onclick="deleteTask(${task.id})" class="dropdown-item" href="#"><i class="fa-solid fa-trash-can"></i> Sil</a></li>
+                            <li><a onclick='editTask(${task.id}, "${task.taskName}")' class="dropdown-item" href="#"><i class="fa-solid fa-pen"></i> Düzenle</a></li>
+                        </ul>
+                    </div>
+                </li>
+            `;
+            
+            ul.insertAdjacentHTML("beforeend", li);           
+
+        }
     }
 }
 
 document.querySelector("#btnAddNewTask").addEventListener("click", newTask);
-document.querySelector("#btnAddNewTask").addEventListener(onkeydown, function(){
-    if(KeyboardEvent == "Enter"){
+document.querySelector("#btnAddNewTask").addEventListener("keypress", function(){
+    if (event.key == "Enter") {
         document.getElementById("btnAddNewTask").click();
     }
 });
 
-function newTask(event){
-    let taskInput = document.getElementById("txtTaskName");
+function newTask(event) {
 
-    if(taskInput.value == ""){
-        alert("You must enter task!");
-    }
-    else{
-        taskList.push({"id": taskList.length + 1, "taskName": taskInput.value});
+    if(taskInput.value == "") {
+        alert("You must enter new task!");
+    } else {
+        if(!isEditTask) {
+            // add
+            taskList.push({"id": taskList.length + 1, "taskName": taskInput.value});
+        } else {
+            // update
+            for(let task of taskList) {
+                if(task.id == editId) {
+                    task.taskName = taskInput.value;
+                }
+                isEditTask = false;
+            }
+        }
+        taskInput.value = "";
         displayTasks();
     }
 
-    
-
     event.preventDefault();
+}       
+
+function deleteTask(id) {
+
+    let deletedId;
+    
+    for(let index in taskList) {
+        if(taskList[index].id == id) {
+            deletedId = index;
+        }
+    }
+
+    taskList.splice(deletedId, 1);
+    displayTasks();
 }
+
+function editTask(taskId, taskName) {
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = taskName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+
+    console.log("edit id:", editId);
+    console.log("edit mode", isEditTask);
+}
+
+btnClear.addEventListener("click", function() {
+    taskList.splice(0, taskList.length);
+    displayTasks();
+})
