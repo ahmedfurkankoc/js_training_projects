4663beb9069dcde5f045a8251ea1349530dc0ea3
Ahmed Furkan Koc <ahmedfurkankoc@gmail.com>
2024-02-12 21:27:07 +0300

    Movie app initial commit

---
+++ MovieApp/script.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=770cb7916f6c76097b77e515d9b2ce1b&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280' 
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=770cb7916f6c76097b77e515d9b2ce1b&query="'
